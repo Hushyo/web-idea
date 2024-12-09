d8ed1721bbbf2ae30ecb6db442ea5d572bc8f03e
Hushyo <Hushsama@hotmail.com>
2024-12-09 19:45:56 +0800

    delete

--- Design/src/main/resources/test.sql
+++
@@ -1,6 +0,0 @@
-create table if not exists `user`(
-    id int  not null,
-    name varchar(30) not null,
-    insert_time datetime not null default current_timestamp,
-    update_time datetime not null default current_timestamp on update current_timestamp
-)
--- README.md
+++
@@ -1,2 +0,0 @@
-# web-workplace
-Web
